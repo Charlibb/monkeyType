b123f1a57ee6e0aa5584f134d3dd16bb8268d314
Carlos B Borruel <zatoichihanzo@gmail.com>
2024-04-12 23:47:50 +0200

    added initGame logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // $ indicates it is part of the DOM element
 const $time = document.querySelector('time');
-const $paragraph = document.querySelector('paragraph');
+const $paragraph = document.querySelector('p');
 const $input = document.querySelector('input');
 
 const INITIAL_TIME = 30;
@@ -23,12 +23,41 @@ function initGame() {
   $paragraph.innerHTML = words
     .map((word, index) => {
       const letters = word.split('');
-      return ` <word>
-    ${letters.map((letter) => `<letter>${letter}</letter>`).join('')}
-    </word>
+      return ` <m-word>
+    ${letters.map((letter) => `<m-letter>${letter}</m-letter>`).join('')}
+    </m-word>
     `;
     })
     .join('');
+
+  const $firstWord = $paragraph.querySelector('m-word');
+  $firstWord.classList.add('active');
+  $firstWord.querySelector('m-letter').classList.add('active');
+
+  const intervalId = setInterval(() => {
+    currentTime--;
+    $time.textContent = currentTime;
+
+    if (currentTime === 0) {
+      clearInterval(intervalId);
+      gameOver();
+    }
+  }, 15000);
 }
 
-function initEvents() {}
+function initEvents() {
+  document.addEventListener('keydown', () => {
+    $input.focus();
+  });
+  $input.addEventListener('keydown', onKeyDown);
+  $input.addEventListener('keyup', onKeyUp);
+}
+
+function onKeyDown() {}
+
+function onKeyUp() {}
+
+function gameOver() {
+  alert('GAME OVER!!!!');
+  $paragraph.innerHTML = 'GAMEOVER!';
+}
